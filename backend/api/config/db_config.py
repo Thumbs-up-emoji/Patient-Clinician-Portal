8ec9f3852d3ee011898ed383fa13dfbde9552cda
Bashskull <f20221539@goa.bits-pilani.ac.in>
2025-07-12 13:54:50 +0530

    requirements wali file added

--- backend/api/config/db_config.py
+++ backend/api/config/db_config.py
@@ -7,8 +7,8 @@ load_dotenv()
 def get_db_connection():
     connection = pymysql.connect(
         host=os.getenv('DB_HOST', 'localhost'),
-        user=os.getenv('DB_USER', 'zero'),
-        password=os.getenv('DB_PASSWORD'),
+        user=os.getenv('DB_USER', 'root'),
+        password=os.getenv('DB_PASSWORD','MySQL#4975'),
         database=os.getenv('DB_NAME', 'patient_portal'),
         cursorclass=pymysql.cursors.DictCursor
     )
