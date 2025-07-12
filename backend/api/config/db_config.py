f25c05a7329a70f6a94cc1880c52556daee3f155
Bashskull <f20221539@goa.bits-pilani.ac.in>
2025-07-12 18:38:14 +0530

    tried adding new user

--- backend/api/config/db_config.py
+++ backend/api/config/db_config.py
@@ -7,8 +7,8 @@ load_dotenv()
 def get_db_connection():
     connection = pymysql.connect(
         host=os.getenv('DB_HOST', 'localhost'),
-        user=os.getenv('DB_USER', 'root'),
-        password=os.getenv('DB_PASSWORD','MySQL#4975'),
+        user=os.getenv('DB_USER', 'one'),
+        password=os.getenv('DB_PASSWORD'),
         database=os.getenv('DB_NAME', 'patient_portal'),
         cursorclass=pymysql.cursors.DictCursor
     )
